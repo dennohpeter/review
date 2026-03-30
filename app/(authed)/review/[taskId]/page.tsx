6f14c7823a7935ac851d0093cf173c2dd1157c75
Dennoh Peter <mail@dennohpeter.com>
2026-03-30 13:38:39 -0500

    fix use params warning

--- app/(authed)/review/[taskId]/page.tsx
+++ app/(authed)/review/[taskId]/page.tsx
@@ -1,14 +1,5 @@
-'use client'
-
-import { use } from 'react'
 import { ReviewInterface } from '@/app/pages/ReviewInterface'
 
-export default function ReviewPage({
-  params,
-}: {
-  params: Promise<{ taskId: string }>
-}) {
-  const { taskId } = use(params)
-
-  return <ReviewInterface taskId={taskId} />
+export default function ReviewPage({ params }: { params: { taskId: string } }) {
+  return <ReviewInterface taskId={params.taskId} />
 }
